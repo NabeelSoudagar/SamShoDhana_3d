34184e13e60d5305fb307e8df1fb3463adad9e81
NabeelSoudagar <nabeelsoudagar1050@gmail.com>
2026-02-06 12:44:39 +0530

    fixed the deployment error

--- main.jsx
+++ main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import ThemeToggle from "./components/ThemeToggle";
-import "./styles/theme.css";
+import "./styles/Theme.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
